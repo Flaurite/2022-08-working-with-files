34f5f26f9e523ddca53bf0fe8fa498b76e47fb7c
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-08-10 11:16:42 +0400

    Complete

--- src/main/java/com/company/jmixpm/screen/task/TaskEdit.java
+++ src/main/java/com/company/jmixpm/screen/task/TaskEdit.java
@@ -2,6 +2,9 @@ package com.company.jmixpm.screen.task;
 
 import com.company.jmixpm.app.TaskService;
 import com.company.jmixpm.entity.Task;
+import io.jmix.ui.component.BrowserFrame;
+import io.jmix.ui.component.FileStorageResource;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,8 +15,36 @@ public class TaskEdit extends StandardEditor<Task> {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private BrowserFrame attachmentBrowserFrame;
+
     @Subscribe
     public void onInitEntity(InitEntityEvent<Task> event) {
         event.getEntity().setAssignee(taskService.findLeastBusyUser());
     }
+
+    @Subscribe(id = "taskDc", target = Target.DATA_CONTAINER)
+    public void onTaskDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Task> event) {
+        if ("attachment".equals(event.getProperty())) {
+            refreshAttachmentPreview();
+        }
+    }
+
+    @Subscribe(id = "taskDc", target = Target.DATA_CONTAINER)
+    public void onTaskDcItemChange(InstanceContainer.ItemChangeEvent<Task> event) {
+        refreshAttachmentPreview();
+    }
+
+    private void refreshAttachmentPreview() {
+        Task task = getEditedEntity();
+        if (task.getAttachment() != null) {
+            attachmentBrowserFrame.setSource(FileStorageResource.class)
+                    .setFileReference(task.getAttachment())
+                    .setMimeType(task.getAttachment().getContentType());
+        }
+    }
+
+
+
+
 }
